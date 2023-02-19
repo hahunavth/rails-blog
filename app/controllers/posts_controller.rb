c7fffd8194e95e85f4f71a0b060f4650aa5b3cfb
hahunavth <vuthanhha.2001@gmail.com>
2023-02-19 10:43:01 +0700

    Change ruby version and add image to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -72,6 +72,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :tag_list)
+      params.require(:post).permit(:title, :body, :tag_list, :image)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,9 @@ class Post < ApplicationRecord
     validates :body, presence: true, length: { minimum:10, maximum:1500 }
     belongs_to :user, foreign_key: 'user_id'
     has_many :comments
+    has_one_attached :image
     acts_as_taggable_on :tags
-    def self.search(search)  
+    def self.search(search)
         where("lower(users.username) LIKE :search OR lower(posts.title) LIKE :search OR lower(posts.body) LIKE :search", search: "%#{search.downcase}%")
     end
 end
